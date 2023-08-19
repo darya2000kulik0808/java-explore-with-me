d4ea335cfc56099ed592ee7c9a7a861ca6a246c7
Darya Kulikova <ivolgadaria@yandex.ru>
2023-08-19 10:48:56 +0300

    fix: убраны версии зависимостей в пом-файлах, дабвлены ограничения на строковые поля для endpoint hit

--- ewm-stat-service/dto/src/main/java/ru/practicum/EndpointHitDto.java
+++ ewm-stat-service/dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -2,6 +2,7 @@ package ru.practicum;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -14,10 +15,13 @@ import java.time.LocalDateTime;
 @Builder
 public class EndpointHitDto {
     @NotBlank(message = "Название приложения не может быть пустым.")
+    @Length(max = 256)
     String app;
     @NotBlank(message = "Путь к эндпоинту приложения не может быть пустым.")
+    @Length(max = 512)
     String uri;
     @NotBlank(message = "IP-адрес, с которого был сделан запрос к эндпоинту приложения не может быть пустым.")
+    @Length(max = 45)
     String ip;
     @NotNull(message = "Время обращения не может быть пустым.")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
